d937670e204d3423c1de07533ebcf893973aad5f
Viktor Tovarnykh <vtovarnykh@gmail.com>
2024-05-01 17:03:43 +0200

    Added banner

--- BonsaiClub/frontend/green-club/src/App.js
+++ BonsaiClub/frontend/green-club/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Banner from "./components/Banner";
 
 function App() {
     return (
         <Router>
             <Header />
+            <Banner />
             <Routes>
                 <Route path="/" element={<Home />} />
                 {/* Other routes here */}
