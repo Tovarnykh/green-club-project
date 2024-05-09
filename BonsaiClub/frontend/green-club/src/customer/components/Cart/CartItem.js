e0a0e009f7484d39884335236fa043ce49164dde
Viktor Tovarnykh <vtovarnykh@gmail.com>
2024-05-09 14:56:56 +0200

    Added Cart

--- BonsaiClub/frontend/green-club/src/customer/components/Cart/CartItem.js
+++ BonsaiClub/frontend/green-club/src/customer/components/Cart/CartItem.js
@@ -1,35 +1,48 @@
 import React from 'react';
+import {Button, IconButton} from "@mui/material";
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 const CartItem = () => {
     return (
         <div className='p-5 shadow-lg border rounded-md'>
             <div className='flex items-center'>
-                <div className='w-[5rem] h-[5rem] lg:w-[9rem] lg:[9rem]'>
+                <div className='w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem]'>
                     <img className='w-full h-full object-cover object-top'
-                        src="https://plnts.com/_next/image?url=https%3A%2F%2Fwebshop.plnts.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2Faa5d334f459227518b6c3cf7ea9d29ed%2Fp%2Fl%2Fpl.m.138-thumbnail.jpg&w=640&q=80"
-                        alt=""/>
+                         src="https://plnts.com/_next/image?url=https%3A%2F%2Fwebshop.plnts.com%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2Faa5d334f459227518b6c3cf7ea9d29ed%2Fp%2Fl%2Fpl.m.138-thumbnail.jpg&w=640&q=80"
+                         alt=""/>
                 </div>
 
                 <div className='ml-5 space-y-1'>
                     <p className='font-semibold'>Plant</p>
                     <p className='opacity-70'>Size: S</p>
-                    <p className='opacity-70 mt-2'>Seller</p>
+                    <p className='opacity-70 mt-2'>Seller: teeeeeext</p>
+                    <div className='flex space-x-5 items-center text-gray-900 pt-6'>
+                        <p className='font-semibold'>
+                            ₴499
+                        </p>
+                        <p className='opacity-50 line-through'>
+                            ₴1499
+                        </p>
+                        <p className='text-green-600 font-semibold'>
+                            66% OFF
+                        </p>
+                    </div>
                 </div>
-
-                <div className='flex space-x-5 items-center text-lg lg:text-x1 text-gray-900 mt-6'>
-
-                    <p className='font-semibold'>
-                        ₴1499
-                    </p>
-                    <p className='opacity-50 line-through'>
-                        ₴499
-                    </p>
-                    <p className='text-green-600 font-semibold'>
-                        66% OFF
-                    </p>
-
+            </div>
+            <div className='lg:flex items-center lg:space-x-10 pt-4'>
+                <div className='flex items-center space-x-2'>
+                    <IconButton >
+                        <RemoveCircleOutlineIcon/>
+                    </IconButton>
+                    <span className='py-1 px-7 border rounded-sm'>3</span>
+                            <IconButton sx={{color:"RGB(145 85 253)"}}>
+                            <AddCircleOutlineIcon/>
+                        </IconButton>
                 </div>
-
+                <div>
+                    <Button sx={{color:"RGB(145 85 253)"}}>Remove</Button>
+                </div>
             </div>
         </div>
     );
--- BonsaiClub/frontend/green-club/src/customer/components/Navigation/Navigation.jsx
+++ BonsaiClub/frontend/green-club/src/customer/components/Navigation/Navigation.jsx
@@ -413,18 +413,6 @@ export default function Navigation() {
                                     </a>
                                 </div>
 
-                                <div className="hidden lg:ml-8 lg:flex">
-                                    <a href="#" className="flex items-center text-gray-700 hover:text-gray-800">
-                                        <img
-                                            src="https://tailwindui.com/img/flags/flag-canada.svg"
-                                            alt=""
-                                            className="block h-auto w-5 flex-shrink-0"
-                                        />
-                                        <span className="ml-3 block text-sm font-medium">CAD</span>
-                                        <span className="sr-only">, change currency</span>
-                                    </a>
-                                </div>
-
                                 {/* Search */}
                                 <div className="flex lg:ml-6">
                                     <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
