a020a60b58f8bea2858d3346259e46992c35918a
Viktor Tovarnykh <vtovarnykh@gmail.com>
2024-05-09 16:31:08 +0200

    Implemented pipe line of product buying process

---
+++ BonsaiClub/frontend/green-club/src/customer/components/AddressCard/AddressCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AddressCard = () => {
+    return (
+        <div className='ml-5'>
+            <div className='space-y-3'>
+                <p className='font-semibold'>Viktor Tovarnykh</p>
+                <p>Kiev, Darnytskiy district, 4001</p>
+                <div className='space-y-1'>
+                    <p className='fonts-semibold'>Phone Number</p>
+                    <p>+380506090077</p>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default AddressCard;
