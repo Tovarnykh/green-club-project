92d8961aca2840d5ae680874c9cd74104d3f937c
Viktor Tovarnykh <vtovarnykh@gmail.com>
2024-05-10 15:08:12 +0200

    Configurate Jwt

---
+++ BonsaiClub/src/main/java/ua/com/kievgreenclub/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package ua.com.kievgreenclub.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="fknqwfknwqjvdpnq fdnsknfklenfqlk";
+    public static final String JWT_HEADER="Authorization";
+
+}
